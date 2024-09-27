0141c94ee5c1ffc4a9485fdf7de017c71b3dc387
cecat <cecatlett@gmail.com>
2024-09-27 10:55:47 -0500

    more bypasses

--- addons/yamcam3/yamcam_functions.py
+++ addons/yamcam3/yamcam_functions.py
@@ -131,7 +131,9 @@ def analyze_audio_waveform(waveform):
 
             # Get and store the output scores
             scores = interpreter.get_tensor(output_details[0]['index'])
-            logger.debug(f"Scores shape: {scores.shape}, Scores: {scores}")
+            #debug
+            #logger.debug(f"Scores shape: {scores.shape}, Scores: {scores}")
+            logger.debug("scores came back, let's not print them")
 
             if scores.size == 0:
                 logger.error("Scores tensor is empty.")
