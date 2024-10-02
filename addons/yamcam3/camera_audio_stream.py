db3d942ea881e13c9a4b2f1dabe67f80f65e61e1
cecat <cecatlett@gmail.com>
2024-10-02 16:12:23 -0500

    clean up log msg format for readability in the firehose

--- addons/yamcam3/camera_audio_stream.py
+++ addons/yamcam3/camera_audio_stream.py
@@ -2,9 +2,35 @@
 #
 # camera_audio_stream.py --> audio streaming class
 #
+#
+#  Class: CameraAudioStream - each sound source is analyzed in a separate thread
+#
+#  Methods:
+#
+#         def __init__(self, camera_name, rtsp_url, analyze_callback)
+#             Set up thread
+#
+#         def start(self)
+#             Start thread - set up FFMPEG to stream with proper settings
+#
+#         def read_stderr(self)
+#             Monitor stderr for messages from FFMPEG which can be informational
+#             or errors, but FFMPEG does not provide a code to differentiate between them.
+#
+#         def stop(self)
+#             Stop thread
+#
+#         def read_stream(self)
+#             Continuously pull data from FFMPEG stream.  When a 31,200 byte segment
+#             is in hand, convert to a form that YAMNet can classify.
+#             Pass the waveform to analyze_callback (in yamnet.py) which
+#             in turn calls rank_scores (in yamnet_functions.py) and returns
+#             results that can be sent (via the report function in yamnet_functions.py)
+#             to Home Assistant via MQTT.
+#             
+#
 
 import subprocess
-#import time
 import threading
 import numpy as np
 import logging
@@ -27,7 +53,6 @@ class CameraAudioStream:
             self.running = False
             self.buffer_size = 31200  # YAMNet needs 15,600 samples, 2B per sample
             self.lock = threading.Lock()
-            #self.stderr_thread = None
             self.analyze_callback = analyze_callback
         except Exception as e:
             logger.error(f"{self.camera_name}: Exception in CameraAudioStream __init__: {e}")
@@ -37,6 +62,7 @@ class CameraAudioStream:
             if self.running:
                 return  # Prevent double-starting
 
+            # construct the FFMPEG command to stream audio from the RTSP path
             command = [
                 'ffmpeg',
                 '-rtsp_transport', 'tcp',
@@ -64,16 +90,16 @@ class CameraAudioStream:
                 )
                 self.running = True
 
-                # Start the reading threads
+                # Start this thread to read from the FFMPEG stream 
                 self.thread = threading.Thread(target=self.read_stream, daemon=True)
                 self.thread.start()
                 self.stderr_thread = threading.Thread(target=self.read_stderr, daemon=True)
                 self.stderr_thread.start()
 
-                logger.info(f"{self.camera_name}: Started audio stream.")
+                logger.info(f"START audio stream: {self.camera_name}:.")
 
             except Exception as e:
-                logger.error(f"{self.camera_name}: Exception in CameraAudioStream start: {e}")
+                logger.error(f"Exception in start.CameraAudioStream: {self.camera_name}: {e}")
                 self.running = False
 
     def read_stderr(self):
@@ -81,10 +107,9 @@ class CameraAudioStream:
             try:
                 stderr_output = self.process.stderr.read(1024).decode()
                 if stderr_output:
-                    logger.debug(f"{self.camera_name}: FFmpeg stderr: {stderr_output}")
+                    logger.debug(f"FFmpeg stderr: {self.camera_name}:  {stderr_output}")
             except Exception as e:
-                logger.error(f"{self.camera_name}: Exception in CameraAudioStream read_stderr: {e}")
-            # No lock needed here as this is an independent read operation
+                logger.error(f"Exception in read-stderr.CameraAudioStream: {self.camera_name}: {e}")
 
     def stop(self):
         with self.lock:
@@ -95,35 +120,32 @@ class CameraAudioStream:
                 self.process.terminate()
                 self.process.wait()
                 self.process = None
-            logger.info(f"{self.camera_name}: Stopped audio stream.")
+            logger.info(f"******-->STOP audio stream: {self.camera_name}.")
 
     def read_stream(self):
-        # Only lock where necessary for shared resources like self.running or self.process
         raw_audio = b""
-        #buffer_size = self.buffer_size  # No need to lock this
 
         while self.running:
             try:
                 while len(raw_audio) < self.buffer_size:
                     chunk = self.process.stdout.read(self.buffer_size - len(raw_audio))
                     if not chunk:
-                        logger.error(f"{self.camera_name}: Exception in CameraAudioStream read_strea(1)m: {e}")
+                        logger.error(f"Exception in read_stream.CameraAudioStream: {self.camera_name}: {e}")
                         break
                     raw_audio += chunk
 
                 if len(raw_audio) < self.buffer_size:
-                    logger.error(f"{self.camera_name}: Exception in CameraAudioStream read_stream(2): {e}")
+                    logger.error(f"Exception in read_stream.CameraAudioStream: {self.camera_name}: {e}")
                     logger.error(f"--->{self.camera_name}: Incomplete audio capture. Total buffer size: {len(raw_audio)}")
                 else:
                     waveform = np.frombuffer(raw_audio, dtype=np.int16) / 32768.0
                     waveform = np.squeeze(waveform)
                     if self.analyze_callback:
-                        #self.analyze_callback(self.camera_name, waveform)
                         self.analyze_callback(self.camera_name, waveform, self.interpreter, self.input_details, self.output_details)
 
 
             except Exception as e:
-                logger.error(f"{self.camera_name}: Exception in CameraAudioStream read_stream(3): {e}")
+                logger.error(f"Exception in read_stream.CameraAudioStream: {self.camera_name}: {e}")
                 logger.error(f"--->{self.camera_name}: Error reading stream: {e}")
                 self.stop()
 
