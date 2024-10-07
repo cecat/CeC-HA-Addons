daf848d7b299d24663a8d621e322d000d3261c39
cecat <cecatlett@gmail.com>
2024-10-07 13:42:30 -0500

    add 5s timeout to ffmpeg command

--- addons/yamcam3/camera_audio_stream.py
+++ addons/yamcam3/camera_audio_stream.py
@@ -72,6 +72,7 @@ class CameraAudioStream:
             self.command = [
                 'ffmpeg',
                 '-rtsp_transport', 'tcp',
+                '-stimeout', '5000000',  # 5s timeout
                 '-i', self.rtsp_url,
                 '-f', 's16le',
                 '-acodec', 'pcm_s16le',
