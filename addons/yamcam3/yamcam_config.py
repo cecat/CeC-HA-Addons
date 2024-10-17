a807f3cea82a467da58bd5ed10a26ccd248a08a3
cecat <cecatlett@gmail.com>
2024-10-17 01:52:48 -0500

    remove filename from log entries- no need

--- addons/yamcam3/yamcam_config.py
+++ addons/yamcam3/yamcam_config.py
@@ -61,10 +61,12 @@ def check_storage(directory, file_extension):
      # -------- SET INITIAL LOGGING LEVEL
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    #format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    format='%(asctime)s - %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
 logger = logging.getLogger(__name__)
+
 
 #                                              #
 ### ---------- SHUT_DOWN HANDLING -----------###
